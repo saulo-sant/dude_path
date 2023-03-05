e57cde3801701ac96c9bfb8979ec42513d0614a0
santn <saulo.santn@gmail.com>
2023-03-05 13:03:33 +0100

    create healthcheck and dependency dc

--- src/index.js
+++ src/index.js
@@ -1,23 +1,27 @@
 const express = require('express');
 var bodyParser = require('body-parser')
+const pgp = require("pg-promise")({});
+// https://expressjs.com/en/guide/database-integration.html#postgresql
+// https://blog.logrocket.com/crud-rest-api-node-js-express-postgresql/
+
+const jsonParser = bodyParser.json()
+const app = express();
+const port = 23000;
 const databaseConfig= {
-  "host": "localhost",
-  "port": 5431,
+  "host": "127.0.0.1",
+  "port": 5432,
   "database": "postgres",
   "user": "postgres",
   "password": "pass"
 };
 
-const pgp = require("pg-promise")({});
-const db = pgp(databaseConfig);
+const connectDB = async () => {
+  const db = await pgp(databaseConfig);
+  db.none('CREATE TABLE test (slug VARCHAR(50) PRIMARY KEY, content VARCHAR(50));')
+}
 
-const jsonParser = bodyParser.json()
-const app = express();
 
-//db.none('CREATE TABLE test (slug VARCHAR(50) PRIMARY KEY, content VARCHAR(50));')
-// https://expressjs.com/en/guide/database-integration.html#postgresql
-// https://blog.logrocket.com/crud-rest-api-node-js-express-postgresql/
-const port = 23000;
+// CRUD SERVICES
 
 app.get('/api/health', (req, res) => {
     res.send({status:"ok"});
@@ -26,27 +30,21 @@ app.get('/api/health', (req, res) => {
 
 app.get('/api/content', async (req, res) => {
   result = await db.many('SELECT * FROM test')
-  console.log(result)
   res.send(result)
 })
 
 app.get('/api/content/:slug', async (req, res) => {
   const {slug} = req.params
   result = await db.one(`SELECT * FROM test WHERE slug='${slug}'`)
-  console.log(result)
   res.send(result)
 })
 
 app.post('/api/content', jsonParser, async (req, res) => {
-  console.log(req.body)
   const n = req.body.length;
-  console.log(n)
-  console.log( `(${req.body[0].slug},${req.body[0].content})`)
   const values = req.body.reduce((acc, obj, idx) => acc + `('${obj.slug}','${obj.content}'),`, '')
-  
-  
-  console.log(`INSERT INTO test (slug, content) VALUES ${values};`)
   await db.none(`INSERT INTO test (slug, content) VALUES ${values.slice(0, values.length - 1)};`);
 })
 
 app.listen(port, () => console.log(`Server is running on http://localhost:${port}`));
+
+connectDB();
