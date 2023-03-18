53e26bd3be804cb433431ee9ab2884370788b5dd
santn <saulo.santn@gmail.com>
2023-03-18 20:09:28 +0100

    fix host issue

--- src/index.js
+++ src/index.js
@@ -8,17 +8,17 @@ const jsonParser = bodyParser.json()
 const app = express();
 const port = 23000;
 const databaseConfig= {
-  "host": "127.0.0.1",
+  "host": "postgres",
   "port": 5432,
   "database": "postgres",
   "user": "postgres",
   "password": "pass"
 };
 
-const connectDB = async () => {
-  const db = await pgp(databaseConfig);
-  db.none('CREATE TABLE test (slug VARCHAR(50) PRIMARY KEY, content VARCHAR(50));')
-}
+
+const db = pgp(databaseConfig);
+db.none('CREATE TABLE test (slug VARCHAR(50) PRIMARY KEY, content VARCHAR(50));')
+
 
 
 // CRUD SERVICES
@@ -40,11 +40,12 @@ app.get('/api/content/:slug', async (req, res) => {
 })
 
 app.post('/api/content', jsonParser, async (req, res) => {
-  const n = req.body.length;
   const values = req.body.reduce((acc, obj, idx) => acc + `('${obj.slug}','${obj.content}'),`, '')
   await db.none(`INSERT INTO test (slug, content) VALUES ${values.slice(0, values.length - 1)};`);
+  res.send({status: "everything added"})
+
 })
 
 app.listen(port, () => console.log(`Server is running on http://localhost:${port}`));
 
-connectDB();
+
